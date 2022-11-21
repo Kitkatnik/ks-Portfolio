3b1ccd00fef3913837701e205906dbd9bf39ded7
Katya <kittyserenakat@gmail.com>
2022-11-21 17:38:21 -0500

    Style explorer sidebar

--- src/components/explorer/explorer.component.jsx
+++ src/components/explorer/explorer.component.jsx
@@ -1,7 +1,9 @@
 import './explorer.styles.scss'
+import vscodepets from '../../assets/vscodepets.png';
 
 import { 
     VscEllipsis,
+    VscChevronRight,
     VscChevronDown,
     VscAccount, 
     VscFolder, 
@@ -26,32 +28,142 @@ import {
 const Explorer = () => {
     return (
             <div className="explorer">
-                <div className="explorer-header">
+                <div className="header">
                     <p>EXPLORER</p>
                     <span><VscEllipsis /></span>
                 </div>
-                <div className="explorer-divider">
-                    <span>
-                        <VscChevronDown />
-                    </span>
-                    <p>PORTFOLIO (WORKSPACE)</p>
-                </div>
-                <div className="explorer-links">
-                    <div className="folder">
-                        <span className="about"><VscAccount /></span>
-                        <p>About</p>
+                <div className="files">
+                    <div className="top">
+                        <div className="divider">
+                            <span><VscChevronRight /></span>
+                            <p>OPEN EDITORS</p>
+                        </div>
+                        <div className="divider shadow">
+                            <span><VscChevronDown /></span>
+                            <p>PORTFOLIO (WORKSPACE)</p>
+                        </div>
+                        <div className="links">
+                            <div className="folder">
+                                <span><VscAccount /></span>
+                                <p>About</p>
+                            </div>
+                            <div className="folder">
+                                <span><VscFolder /></span>
+                                <p>Resume</p>
+                            </div>
+                            <div className="folder">
+                                <span><VscFolder /></span>
+                                <p>Stats</p>
+                            </div>
+                            <div className="folder">
+                                <span><VscFolderOpened /></span>
+                                <p>Projects</p>
+                            </div>
+                            <div className="projects">
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                <div className="folder">
+                                    <span><VscFileCode /></span>
+                                    <p>Links here</p>
+                                </div>
+                                
+                            </div>
+                        </div>
                     </div>
-                    <div className="folder">
-                        <span className="about"><VscFolder /></span>
-                        <p>Resume</p>
-                    </div>
-                    <div className="folder">
-                        <span className="about"><VscFolder /></span>
-                        <p>Stats</p>
-                    </div>
-                    <div className="folder">
-                        <span className="about"><VscFolder /></span>
-                        <p>Projects</p>
+                    <div className="bottom">
+                        <div className="divider">
+                            <span><VscChevronRight /></span>
+                            <p>OUTLINE</p>
+                        </div>
+                        <div className="divider">
+                            <span><VscChevronRight /></span>
+                            <p>TIMELINE</p>
+                        </div>
+                        <div className="divider">
+                            <span><VscChevronRight /></span>
+                            <p>NPM SCRIPTS</p>
+                        </div>
+                        <div className="divider">
+                            <span><VscChevronDown /></span>
+                            <p>VS CODE PETS</p>
+                        </div>
+                        <div>
+                            <img src={vscodepets} />
+                        </div>
                     </div>
                 </div>
             </div>
