45314aa5b7594373c9e3aebf5b6725b39e0f5faf
Katya <kittyserenakat@gmail.com>
2022-12-06 20:09:21 -0500

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 
 // import { requestRepos } from './utils/github/github.utils';
+import { inject } from '@vercel/analytics';
 
 import BrowserWindow from './components/browser-window/browser-window.component';
 import IconBar from './components/iconbar/iconbar.component';
@@ -9,6 +10,8 @@ import About from './routes/about/about.component';
 import StatusBar from './components/status-bar/status-bar.component';
 
 function App() {
+  inject();
+
   // const reposList = requestRepos()
   //   .then( response => response.json())
   //   .then( data => data)
