d1fe526639feb2b821f8731efb2e2afd3d010dc8
Katya <kittyserenakat@gmail.com>
2023-01-07 21:14:39 -0500

    #1 Format about, activities, and skills

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,7 @@
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
 
+
 import './home.styles.scss';
 import KatyaHeadshot from '../../assets/KatyaHeadshot.jpg';
 
@@ -18,20 +19,58 @@ const Home = (test) => {
 
     return (
         <SimpleBar className="about-container">
+            <p className="comment">// HI, I'M KATYA SARMIENTO</p><br />
             <p>
                 <span className="code">import</span> <span className="style">'./professional.styles.scss'</span>;
             </p>
             <p className="active-line">
+                <span className="code">import</span> pronouns <span className="code">from</span> <span className="style">'../../she/they'</span>;
+            </p>
+            <p className="active-line">
                 <span className="code">import</span> <mark className="highlight">myPortrait</mark><span className="blinking-cursor">|</span><span className="code">from</span> <span className="style">'../../assets/myPortrait.jpg'</span>;
             </p>
-            <div className="code-popup">
-                <p>
-                    (alias) <span className="code">const</span> myPortrait<span className="code">:</span> <const className="type">string</const>
-                </p>
-                <p>
-                    <span className="code">import</span> myPortrait
-                </p>
-                <img src={KatyaHeadshot} alt="Headshot" className="img-popup" />
+            <div className="position-container">
+
+                <div className="code-popup">
+                    <p>
+                        (alias) <span className="code">const</span> myPortrait<span className="code">:</span> <const className="type">string</const>
+                    </p>
+                    <p>
+                        <span className="code">import</span> myPortrait
+                    </p>
+                    <img src={KatyaHeadshot} alt="Headshot" className="img-popup" />
+                </div>
+                <div className="background-container">
+                    <div className="background-text-container">
+                        <div className="background-text left">
+                            <div className="back-symbols dots">. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .</div>
+                            <div className="back-symbols dots">. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .</div>
+                        </div>
+                        <div className="background-text">
+                        <p>
+                            <span className="code">
+                                const
+                            </span> <span className="variable">
+                                currently
+                            </span> = (<span className="variable">
+                                job
+                            </span>) ={charCode(codes.greaterThan)} {charCode(codes.leftBrace)} return <span className="style">
+                                'Searching for Software Engineer / Full-Stack Developer roles'
+                            </span> {charCode(codes.rightBrace)};
+                        </p>
+                        <p>
+                            <span className="code">
+                                const
+                            </span> <span className="variable">
+                                location
+                            </span> = {charCode(codes.leftBracket)} <span className="style">'Atlanta, GA'</span>, <span className="style">'Willing to work remotely'</span> {charCode(codes.rightBracket)};
+                        </p>
+                        </div>
+                    </div>
+                    <div className="back-symbols">
+                        ==========================================================================================================================================================================================================
+                    </div>
+                </div>
             </div>
 
             <p>
@@ -43,47 +82,10 @@ const Home = (test) => {
                     skills
                 </span>) ={charCode(codes.greaterThan)} {charCode(codes.leftBrace)}
             </p>
-            <div className="back-symbols dots">. . . . . . . . . . . . . .</div>
-            <div className="back-symbols dots">. . . . . . . . . . . . . .</div>
-            <div className="back-symbols dots">. . . . . . . . . . . . . .</div>
-            <div className="back-symbols dots">. . . . . . . . . . . . . .</div> 
 
-            <br/>
+            <br />
 
             <div className="nested">
-                <p>
-                    <span className="code">
-                        const
-                    </span> <span className="variable">
-                        pronouns
-                    </span> = <span className="style">
-                        'she/they'
-                    </span>;
-                </p>
-                <p>
-                    <span className="code">
-                        const
-                    </span> <span className="variable">
-                        currently
-                    </span> = (<span className="variable">
-                        job
-                    </span>) ={charCode(codes.greaterThan)} {charCode(codes.leftBrace)} return <span className="style">
-                        'Searching for Software Engineer / Full-Stack Developer roles'
-                    </span> {charCode(codes.rightBrace)};
-                </p>
-                <p>
-                    <span className="code">
-                        const
-                    </span> <span className="variable">
-                        location
-                    </span> = <span className="style">
-                        'Atlanta, GA (willing to work remotely)'
-                    </span>;
-                </p>
-                    <div className="back-symbols">
-                        =====================================================================================================
-                    </div>
-                
                 <p>
                     <span className="code">return (</span>
                 </p>
@@ -91,44 +93,65 @@ const Home = (test) => {
                 <br/>
 
                 <div className="nested">
+                    <div className="about-activities-container">
+                        <div className="about-activities-left">
+                            <h1 className="comment">{charCode(codes.forwardSlash)}* ABOUT *{charCode(codes.forwardSlash)}</h1>
+                            <br/>
+                            <p>
+                                Driven Full-Stack Engineer with a strong bias for impact, proficient in using Javascript for frontend and backend programming, React, HTML, CSS, API implementing, Node.JS, Express, SQL, PostgreSQL, and frameworks like Bootstrap and Material UI. Experience as a Founder and Director of Operations provides an advantage in driving key initiatives, maximizing impact, innovating and iterating, working autonomously, acting fast, and being flexible in facilitating critical change for high-growth, multi-million-dollar B2B companies. 
+                            </p>
+                        </div>
+                        <div className="about-activities">
+                            <h1 className="comment">{charCode(codes.forwardSlash)}* ACTIVITIES *{charCode(codes.forwardSlash)}</h1><br/>
+                            <p><span className="background dots">. . . .</span> List awards and hackathons here</p>
+                        </div>
 
-                    <h1 className="comment">{charCode(codes.forwardSlash)}* ABOUT *{charCode(codes.forwardSlash)}</h1><br/>
-                    <p>
-                        Driven Full-Stack Engineer with a strong bias for impact, proficient in using Javascript for frontend and backend programming, React, HTML, CSS, API implementing, Node.JS, Express, SQL, PostgreSQL, and frameworks like Bootstrap and Material UI. Experience as a Founder and Director of Operations provides an advantage in driving key initiatives, maximizing impact, innovating and iterating, working autonomously, acting fast, and being flexible in facilitating critical change for high-growth, multi-million-dollar B2B companies. 
-                    </p>
+                    </div>
 
-                    <h1 className="comment">{charCode(codes.forwardSlash)}* ACTIVITIES *{charCode(codes.forwardSlash)}</h1>
-                    <p><span className="background dots">. . . .</span> List awards and hackathons here</p>
                     <br/><br/>
 
                     <h1 className="comment">{charCode(codes.forwardSlash)}* SKILLS *{charCode(codes.forwardSlash)}</h1><br/>
-                    <p><span className="code">const</span> <span className="variable">skills</span> = {charCode(codes.leftBrace)}</p>
+                    <p>{charCode(codes.leftBrace)}</p>
                     <br/>
                     <div className="nested">
                         <p>
-                            frontend: {charCode(codes.leftBracket)} 
+                            <span className="code">frontend</span>: {charCode(codes.leftBracket)} 
                             <br/>
-                            <div className="nested">array here</div>
+                            <div className="nested">
+                                HTML, CSS, Sass, Styled Components, Javascript, React, Redux, jQuery
+                            </div>
                             {charCode(codes.rightBracket)};<br/>
                         </p>
 
                         <br/>
 
                         <p>
-                            backend: {charCode(codes.leftBracket)} 
+                            <span className="code">backend</span>: {charCode(codes.leftBracket)} 
                             <br/>
-                            <div className="nested">array here</div>
+                            <div className="nested">
+                                Node.js, Express, RESTful APIs, SQL, PostgreSQL</div>
                             {charCode(codes.rightBracket)};<br/>
                         </p>
 
                         <br/>
 
                         <p>
-                            additionalSkills: {charCode(codes.leftBracket)} 
+                            <span className="code">tools</span>: {charCode(codes.leftBracket)} 
                             <br/>
-                            <div className="nested">array here</div>
+                            <div className="nested">Mocha, Chai, Jest, Knex, Git, GitHub, Heroku, Vercel, Netlify, Firebase, Node Package Manager, Yarn, Visual Studio Code, Figma, Photoshop, Miro, Atlassian (Confluence, Jira, Trello), Asana, ClickUp, Teamwork, Basecamp, 15Five, Lattice, Gusto, Slack</div>
                             {charCode(codes.rightBracket)};<br/>
                         </p>
+
+                        <br/>
+
+                        <p>
+                            <span className="code">additionalSkills</span>: {charCode(codes.leftBracket)} 
+                            <br/>
+                            <div className="nested">Project and product management, process documentation, system conversions and integrations, cross-functional collaboration, product-led growth, product analytics, user feedback collection and analysis, design systems, UI and UX design, roadmap planning, customer and community support, and open source contributions.</div>
+                            {charCode(codes.rightBracket)};<br/>
+                        </p>
+
+                        <br/>
 
                     </div>
                     <p>{charCode(codes.rightBrace)}</p><br/>
